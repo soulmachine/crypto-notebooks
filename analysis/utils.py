e0325ee2588f567b3f5d7c1b711d652a6fc6b7fd
soulmachine <soulmachine@gmail.com>
2020-06-22 09:42:57 +0000

    Added a notebook to analyze 10s time bars

---
+++ analysis/utils.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def read_bars(csv_file: str)->pd.DataFrame:
+    dtypes = {'exchange': 'category', 'market_type': 'category', 'pair': 'category', 'bar_type': 'category',
+              'bar_size':'category', 'open': 'float32', 'timestamp': 'int64', 'timestamp_end': 'int64',
+              'high': 'float32', 'low': 'float32', 'close': 'float32', 'mean': 'float32', 'median': 'float32',
+              'volume': 'float64', 'volume_sell': 'float64', 'volume_buy': 'float64', 
+              'volume_quote': 'float64', 'volume_quote_sell': 'float64', 'volume_quote_buy': 'float64',
+              'count': 'int32', 'count_sell': 'int32', 'count_buy': 'int32'}
+    bars_df = pd.read_csv(csv_file, engine='c', dtype=dtypes)
+    bars_df['timestamp'] = pd.to_datetime(bars_df['timestamp'], unit='ms')
+    bars_df['timestamp_end'] = pd.to_datetime(bars_df['timestamp_end'], unit='ms')
+    bars_df.set_index('timestamp', drop=False, inplace=True, verify_integrity=True)
+    return bars_df
